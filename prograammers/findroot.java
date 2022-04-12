2ee439412fc972d30bed3d395fe5584f83c0f14a
kid1493 <34921641+kid1493@users.noreply.github.com>
2022-04-12 20:52:24 +0900

    Update findroot.java

--- prograammers/findroot.java
+++ prograammers/findroot.java
@@ -1,24 +1,13 @@
-package algo;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-public class findroot {
-
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int res = Integer.parseInt(br.readLine());
-		int ans = (int) Math.sqrt(res);
-		int key = ans * ans;
-//		System.out.println(ans);
-		if( key != (res)) {
-			System.out.println("-1");
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+		long ans = (int) Math.sqrt(n);
+		long key = ans * ans;
+		if( key != (n)) {
+			return -1;
 		}else {
-			System.out.println((ans+1)*(ans+1));
+			return ((ans+1)*(ans+1));
 		}
-	
-	}
-
+    }
 }
-
-// 프로그래머스 
+// 프로그래머스 정수 제곱근 구하기
