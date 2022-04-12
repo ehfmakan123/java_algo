c5aa8a832677d6fe65fcfab524174d3e02747eff
kid1493 <34921641+kid1493@users.noreply.github.com>
2022-04-13 08:35:17 +0900

    Update BOJ_1929_Eratostenes.java

--- BOJ/BOJ_1929_Eratostenes.java
+++ BOJ/BOJ_1929_Eratostenes.java
@@ -31,4 +31,44 @@ public class Primenum_BOJ1929 {
 
 }
 
-// 
+// 기존 방식(밑에거)로 하면 시간초과임
+
+
+package Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Primenum_BOJ1929 {
+
+	public static void main(String[] args)throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		
+		for(int i = M ; i< N+1 ; i++) {
+			search(i);
+		}
+	}
+
+	private static void search(int n) {
+		boolean key = false;
+		for(int i = 2 ; i < (n/2)+1 ; i++) {
+			if(n%i == 0) {
+				key = true;
+				break;
+			}
+		}
+		if(!key) {
+			System.out.println(n);
+		}
+	}
+
+}
+
+
+
+
