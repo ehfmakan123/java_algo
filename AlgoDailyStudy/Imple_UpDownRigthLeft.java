5028672375e8196d17a9062f5ff8eb6ac9c87061
kid1493 <34921641+kid1493@users.noreply.github.com>
2022-04-20 09:55:14 +0900

    Update Imple_UpDownRigthLeft.java

--- AlgoDailyStudy/Imple_UpDownRigthLeft.java
+++ AlgoDailyStudy/Imple_UpDownRigthLeft.java
@@ -11,28 +11,28 @@ public class UpDownRigthLeft {
 		System.setIn(new FileInputStream("res/UpDownRigthLeft.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		char key;
+		char key; // 비교값
 		int x = 1 ; int y = 1;
-		String str = br.readLine();
-		for(int i = 0 ; i < str.length() ; i++) {
+		String str = br.readLine(); // 문자열 입력
+		for(int i = 0 ; i < str.length() ; i++) { // 문자를 하나씩 확인
 			key = str.charAt(i);
-			if(key == 'R') {
-				if(y < 5) {
+			if(key == 'R') { // R 이면 오른쪽 한칸
+				if(y < 5) { // 범위 초과 아니면
 					y++;
 				}
-			}else if( key == 'L') {
-				if(y >1) {
+			}else if( key == 'L') { // L 이면 왼쪽 한칸
+				if(y >1) { // 1밑이면 범위 초과
 					y--;
 				}
-			}else if( key == 'U') {
+			}else if( key == 'U') { // 위로 한칸
 				if(x > 1) {
 					x--;
 				}
-			}else if( key == 'D') {
+			}else if( key == 'D') { // 아래로 한칸
 				if( x < 5) {
 					x++;
 				}
-			}else {
+			}else { // 공백 문자 무시
 				continue;
 			}
 		}
