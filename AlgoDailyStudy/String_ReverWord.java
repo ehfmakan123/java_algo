63ef583f343a9c6f91c2fbd5b6b147a44140a96f
kid1493 <34921641+kid1493@users.noreply.github.com>
2022-04-22 00:18:18 +0900

    Create String_ReverWord.java

---
+++ AlgoDailyStudy/String_ReverWord.java
@@ -0,0 +1,19 @@
+package Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ReverseWord {
+
+	public static void main(String[] args)throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for(int i = 0 ; i < N ; i++) {
+			StringBuilder str = new StringBuilder(br.readLine());
+			str.reverse();
+			System.out.println(str);
+		}
+	}
+
+}
