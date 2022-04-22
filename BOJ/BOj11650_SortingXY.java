aca49ff214c59a8ec6cdc451c32c35ae7e8a40dd
kid1493 <34921641+kid1493@users.noreply.github.com>
2022-04-22 22:22:08 +0900

    Create BOj11650_SortingXY.java

---
+++ BOJ/BOj11650_SortingXY.java
@@ -0,0 +1,40 @@
+package Test;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class SortingXY_BOj11650 {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		System.setIn(new FileInputStream("res/SortingXY_Input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int [N][2];
+		
+		for(int i = 0 ; i < N ;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		// 람다식 표현
+		Arrays.sort(arr , (e1 ,e2) ->{
+			if((e1[0] == e2[0])) {
+				return e1[1] - e2[1];
+			}else {
+				return e1[0] - e2[0];
+			}
+		});
+		
+		for(int i = 0 ; i< N ; i++) {
+			System.out.println(arr[i][0] + " " + arr[i][1]);
+		}
+	}
+	
+}
+
+//람다식 표현하기!
