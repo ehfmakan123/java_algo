b546af9f0b11e80527ad21146a7923715555f38f
kid1493 <34921641+kid1493@users.noreply.github.com>
2022-04-22 21:17:02 +0900

    Update Array_CalcScore.java

--- AlgoDailyStudy/Array_CalcScore.java
+++ AlgoDailyStudy/Array_CalcScore.java
@@ -14,19 +14,29 @@ public class CalcScore {
 		int[] arr = new int [N+1];
 		int sum = 0 ;
 		
-		int cnt = 1;
 		
 		for(int i = 0 ; i < N ; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		for(int i = 1 ; i <= N ; i++) {
-			if(arr[i-1] == 1 && arr[i] == 1) {
-				sum+= cnt;
+//		int cnt = 1;
+//		for(int i = 1 ; i <= N ; i++) {
+//			if(arr[i-1] == 1 && arr[i] == 1) {
+//				sum+= cnt;
+//				cnt++;
+//			}else if(arr[i-1] == 1 && arr[i] == 0){
+//				sum += cnt;
+//				cnt = 1;
+//			}
+//		}
+// ############################2번 방범 (더 쉬움 ) ##########################		
+		int cnt = 0;
+		for(int i = 0 ; i < N ; i++) {
+			if(arr[i] == 1) {
 				cnt++;
-			}else if(arr[i-1] == 1 && arr[i] == 0){
 				sum += cnt;
-				cnt = 1;
+			}else {
+				cnt = 0 ;
 			}
 		}
 		System.out.println(sum);
