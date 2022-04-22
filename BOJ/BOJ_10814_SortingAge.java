439cf1cf95feac1d921dcb4a93fe2ffb259bc05c
kid1493 <34921641+kid1493@users.noreply.github.com>
2022-04-22 23:15:26 +0900

    Update BOJ_10814_SortingAge.java

--- BOJ/BOJ_10814_SortingAge.java
+++ BOJ/BOJ_10814_SortingAge.java
@@ -20,13 +20,20 @@ public class SortingAge_BOJ10814 {
 			str[i][0] =st.nextToken();
 			str[i][1] = st.nextToken();
 		}
-		Arrays.sort(str, new Comparator<String[]>() {
-
-			@Override
-			public int compare(String[] o1, String[] o2) {
-				// TODO Auto-generated method stub
-				return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
-			}
+		
+		
+//		Arrays.sort(str, new Comparator<String[]>() {
+//
+//			@Override
+//			public int compare(String[] o1, String[] o2) {
+//				// TODO Auto-generated method stub
+//				return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
+//			}
+//		});
+		
+		
+		Arrays.sort(str , (s1 ,s2)->{
+				return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
 		});
 		
 		for(int i = 0 ;i < N ; i++) {
