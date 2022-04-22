6e0224096ae3e4e7b2e077d75427e832be674943
kid1493 <34921641+kid1493@users.noreply.github.com>
2022-04-22 11:04:38 +0900

    Update String_ExtractNumber.java

--- AlgoDailyStudy/String_ExtractNumber.java
+++ AlgoDailyStudy/String_ExtractNumber.java
@@ -11,19 +11,32 @@ public class ExtractNumber {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
-		Queue<Integer> q = new LinkedList<Integer>();
+// 		Queue<Integer> q = new LinkedList<Integer>();
+// 		for(char c : str.toCharArray()) {
+// 			if(Character.isDigit(c)) {
+// 				int key = c-'0';
+// 				q.offer(key);
+// 			}
+// 		}
+// 		int sum = 0 ;
+// 		while(!q.isEmpty()) {
+// 			sum += q.peek()*(Math.pow(10, q.size()-1));
+// 			q.poll();
+// 		}
+// 		System.out.println(sum);
+// 	}
+	
+//	##################### 2번/3번 째 풀이방법 (더 간단하다!!!)####################
+	int sum = 0 ;
+		String res ="";
 		for(char c : str.toCharArray()) {
 			if(Character.isDigit(c)) {
 				int key = c-'0';
-				q.offer(key);
+				sum = sum *10 + key;
+				res += c;
 			}
 		}
-		int sum = 0 ;
-		while(!q.isEmpty()) {
-			sum += q.peek()*(Math.pow(10, q.size()-1));
-			q.poll();
-		}
-		System.out.println(sum);
+		System.out.println(sum); // int 로 나타내기
+		System.out.println(Integer.parseInt(res)); // String으로 입력 후 integer 변환
 	}
-
 }
