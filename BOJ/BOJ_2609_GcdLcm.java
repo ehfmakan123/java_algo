2c2f23eecd2c6efe0fdb6e64a9d8b0f718f569ec
kid1493 <34921641+kid1493@users.noreply.github.com>
2022-04-25 03:42:16 +0900

    Create BOJ_2609_GcdLcm.java

---
+++ BOJ/BOJ_2609_GcdLcm.java
@@ -0,0 +1,30 @@
+package Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class GcdLcm_BOJ_2609 {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		
+		System.out.println(gcd(Math.max(x, y) , Math.min(x, y)));
+		System.out.println((x * y) / gcd(Math.max(x, y) , Math.min(x, y)));
+	}
+
+	private static int gcd(int x, int y) {
+		int r = x % y;
+		if(r == 0) {
+			return y;
+		}else {
+			return gcd(y,r);
+		}
+		
+	}
+
+}
