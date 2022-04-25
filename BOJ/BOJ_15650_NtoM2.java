9cb42acf421d68ba6e3cb2bba31da706b79d364c
kid1493 <34921641+kid1493@users.noreply.github.com>
2022-04-26 01:29:56 +0900

    Update BOJ_15650_NtoM2.java

--- BOJ/BOJ_15650_NtoM2.java
+++ BOJ/BOJ_15650_NtoM2.java
@@ -1,10 +1,8 @@
-package Test;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class NtoM2_BOJ15650 {
+public class Main {
 static int M , N;
 static int[] arr;
 static StringBuilder sb;
@@ -17,24 +15,24 @@ static StringBuilder sb;
 		M = Integer.parseInt(st.nextToken());
 		arr = new int[M];
 		
-		dfs(1,0);
+		dfs(1,0); // 1부터 시작
 		System.out.println(sb.toString());
 		br.close();
 		
 	}
 	private static void dfs(int st, int depth) {
 		// TODO Auto-generated method stub
-		if(depth == M) {
+		if(depth == M) { // 깊이가 M일때 (종료 조건)
 			for(int i : arr) {
-				sb.append(i+" ");
+				sb.append(i+" "); // 배열에 있는걸 전부 sb에 넣음
 			}
 			sb.append("\n");
 			return;
 		}
 		
-		for(int i = st; i <= N; i++) {
-			arr[depth] = i;
-			dfs(i+1, depth+1);
+		for(int i = st; i <= N; i++) { // 현재 시작점 부터 반복 (중복 X)
+			arr[depth] = i; // depth 번째 배열에 i 입력
+			dfs(i+1, depth+1); // dfs 
 		}
 	}
 
